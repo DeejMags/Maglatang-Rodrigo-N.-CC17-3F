4b0fab18e1ad69e39ec3e1a9f3405ffec2332151
DeejMags <rnmaglatangjr@gmail.com>
2024-09-24 23:58:41 +0800

    idk

---
+++ app/src/main/java/com/example/dicerollerappmaglatang3f/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.dicerollerappmaglatang3f
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Center
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.dicerollerappmaglatang3f.ui.theme.DiceRollerAppMaglatang3FTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRollerAppMaglatang3FTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DiceRollerApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DiceRollerApp(modifier: Modifier = Modifier) {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var diceRoll by remember { mutableStateOf(1) }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Center
+    ) {
+        Image(
+            painter = painterResource(
+                when (diceRoll) {
+                    1 -> R.drawable.dice_1
+                    2 -> R.drawable.dice_2
+                    3 -> R.drawable.dice_3
+                    4 -> R.drawable.dice_4
+                    5 -> R.drawable.dice_5
+                    else -> R.drawable.dice_6
+                }
+            ),
+            contentDescription = diceRoll.toString()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            diceRoll = Random.nextInt(1, 6)
+        }) {
+            Text(text = stringResource(R.string.roll))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewDiceRollerApp() {
+    DiceRollerApp()
+}
